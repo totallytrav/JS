fd8b50b6b154ce8000a65e91fa35d4a82be405b9
totallytrav <52040167+totallytrav@users.noreply.github.com>
2022-07-27 14:46:20 -0500

    Update 8x8ClearlocalStorage.user.js

--- 8x8ClearlocalStorage.user.js
+++ 8x8ClearlocalStorage.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         8x8 Clear localStorage
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.1
 // @description  Clears unwanted cache
 // @author       Travis
 // @match        https://vcc-na15.8x8.com/AGUI/load.php?loginmode=vo
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=8x8.com
-// @downloadURL  https://github.com/totallytrav/JS/8x8ClearlocalStorage.js
 // @grant        none
 // ==/UserScript==
 
