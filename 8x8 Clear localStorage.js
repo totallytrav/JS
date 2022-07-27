982e5974ee93da63dec44c119d2b5a8a6f31153a
totallytrav <52040167+totallytrav@users.noreply.github.com>
2022-07-27 14:41:25 -0500

    Update 8x8 Clear localStorage.js

--- 8x8 Clear localStorage.js
+++ 8x8 Clear localStorage.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         8x8 Clear localStorage
-// @namespace    http://tampermonkey.net/
+// @namespace    http://github.net/
 // @version      1.0
-// @description  try to take over the world!
+// @description  Clears unwanted cache
 // @author       travis.olson
 // @match        https://vcc-na15.8x8.com/AGUI/load.php?loginmode=vo
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=8x8.com
